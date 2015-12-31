e61fe2ab6a96173fccba6af9d6aceda433836b3d
Aran Bergman <aran@yahoo-inc.com>
2015-12-31 18:05:41 +0200

    end of internship commit

--- fresh_start.sh
+++ fresh_start.sh
@@ -11,13 +11,13 @@ hbase shell << EOF
    disable '${TABLE}'
    drop '${TABLE}'
    n_splits = ${splits}
-   create '${TABLE}', '${CF}', {SPLITS => (1..n_splits).map {|i| "user#{1000+i*(9999-1000)/n_splits}"}}
+   create '${TABLE}', '${CF}', {SPLITS => (1..n_splits).map {|i| "user#{i*(10000/(n_splits+1))}"}}
    alter '${TABLE}', {NAME=>'${CF}', VERSIONS=>${VERSIONS}}
    describe '${TABLE}'
 EOF
 
 ssh -T tso << EOF
-   ~/inst/tso-server/bin/omid.sh create-hbase-commit-table -numSplits 16
+   ~/inst/tso-server/bin/omid.sh create-hbase-commit-table -numSplits 3
    ~/inst/tso-server/bin/omid.sh create-hbase-timestamp-table
 EOF
 
